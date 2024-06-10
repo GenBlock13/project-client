44657353243f981997c4a7038efa69474c0d0d73
Reptilius <blockgen@yandex.ru>
2024-06-10 15:32:11 +0300

    Front_2lesson

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,6 +2,6 @@ import cls from './Container.module.scss'
 
 export const Container = ({ children}) => {
     return (
-        <div className={cls.Container}>{ children }</div>
+        <div className={cls.container}>{ children }</div>
     )
 }
