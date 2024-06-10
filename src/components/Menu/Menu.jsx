6dac6f78d823ac0311064252406b018a3f624dd3
Reptilius <blockgen@yandex.ru>
2024-06-10 16:59:45 +0300

    MenuLink

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,14 @@
 import cls from './Menu.module.scss'
+import { MenuLink } from '../ui/MenuLink/MenuLink'
+import { Link } from 'react-router-dom'
 
 export const Menu = () => {
     return (
         <nav className={cls.nav}>
-            Menu
+            <MenuLink to="#places">Достопримечательности</MenuLink>
+            <MenuLink to="#facts">Факты</MenuLink>
+            <MenuLink to="comments">Отзывы</MenuLink>
+            <MenuLink to="#map">На карте</MenuLink>
         </nav>
     )
 }
