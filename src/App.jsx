05780ec33214160e23d4c781da863288e656c577
Reptilius <blockgen@yandex.ru>
2024-06-19 13:30:43 +0300

    Admin added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom"
 import { MainPage, NotFoundPage, AddPlacePage } from "./pages"
-import { Navbar, Page } from './components'
+import { Navbar, Page, ProtectedRoute, Footer } from './components'
 import { useEffect } from 'react'
 import { useStore } from './store/StoreProvider'
 import { observer } from 'mobx-react-lite'
-import { Footer } from './components'
+
 
 
 function App() {
@@ -20,13 +20,18 @@ function App() {
   if (authStore.isLoading) {
     return <div>Loading..</div>
   }
+
+  const isAllowedAdmin = authStore.user.role === 'ADMIN' || localStorage.getItem('roleAdmin')
+
   return (
     <>
     <Navbar />
     <Page>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route element={<ProtectedRoute isAllowed={isAllowedAdmin} />} >
         <Route path="/add-place" element={<AddPlacePage />} />
+        </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Page>
--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -1,9 +1,24 @@
-import { Section } from '../'
+import { Section, Text, LinkButton, buttonType } from '../'
+import cls from './Places.module.scss'
+import { useStore } from '../../store/StoreProvider'
 
 export const Places = () => {
+    const { authStore } = useStore()
     return (
-        <Section id={'places'}>
-            Places
+        <Section className={cls.places} id={'places'}>
+            <div className={cls.header}>
+                <Text bold color='black' className={cls.title}
+                    title='Интересные места' size='s' align='left' />
+                {
+                    authStore.user.role === 'ADMIN' &&
+                    <LinkButton
+                        to={'/add-place'}
+                        variant={buttonType.FILLED}
+                    >
+                        Добавить +
+                    </LinkButton>
+                }
+            </div>
         </Section>
     )
 }
